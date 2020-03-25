be49362c76164186277d032c4cf9329cbb1fcf60
sergiovill1 <sergioconsejo2@gmail.com>
2020-03-24 23:34:31 -0500

    main

--- src/directorio/Directorio.java
+++ src/directorio/Directorio.java
@@ -12,7 +12,7 @@
     import org.w3c.dom.NodeList;
     import org.xml.sax.SAXException;
      
-    public class directorio {
+    public class Directorio {
      
         public static void main(String[] args) {
      
